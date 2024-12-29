a66341f7e9ff5d3c138e73c32bf85219d1b60bf2
PepechiMinmi <p5420077@stu.chs.nihon-u.ac.jp>
2024-12-29 21:05:20 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-//ホーム画面：プロンプトを作成ボタンとなにか
-
 import Link from 'next/link';
+
+
+const mainStyle: React.CSSProperties = {
+  textAlign: 'center',
+};
+
+const buttonStyle: React.CSSProperties = {
+  width: '200px',
+  height: '50px',
+  fontSize: '16px',
+  padding: '10px',//内側の余白
+  backgroundColor: '#007BFF',//ボタンの背景色
+  color: '#fff',//ボタンの文字色
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+};
 
 export default function Home() {
   return (
@@ -9,10 +24,7 @@ export default function Home() {
         <h1>背景プロンプター</h1>
         <h2>AI画像生成用の背景プロンプトを簡単に作成しよう！</h2>
 
-
-        <p>
-          「プロンプトを作成するボタン」を押してください．
-        </p>
+        <p>「プロンプトを作成するボタン」を押してください．</p>
         <Link href="/prompter">
           <button style={buttonStyle}>プロンプトを作成する</button>
         </Link>
@@ -20,19 +32,3 @@ export default function Home() {
     </div>
   );
 }
-
-const mainStyle = {
-  textAlign: 'center',
-};
-
-const buttonStyle = {
-  width: '200px',     
-  height: '50px',     
-  fontSize: '16px',   
-  padding: '10px',    // 内側の余白
-  backgroundColor: '#007BFF', // ボタンの背景色
-  color: '#fff',      // ボタンの文字色
-  border: 'none',     
-  borderRadius: '5px',
-  cursor: 'pointer',  
-};
